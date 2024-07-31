6c94b685fbec2e28297a31344f580ab7867cb2c1
Ahmed <ahmedelsharawy117@gmail.com>
2024-08-01 00:45:42 +0300

    chore: main router

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func getHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, stranger..."))
+}
+
 func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", getHello)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 
 	// Start the server on port 8080
 	fmt.Println("[server]: starting on localhost:8080")
@@ -16,4 +29,9 @@ func main() {
     ╚██████╔╝╚█████╔╝  ██║░░██║██║░░░░░██║
     ░╚═════╝░░╚════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝`)
 
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+		return
+	}
 }
