6e35a5e40601c71445b75be0f096f6e736c7ea3b
Ahmed <ahmedelsharawy117@gmail.com>
2024-10-01 12:37:44 +0300

    setup database

--- database/db.go
+++ database/db.go
@@ -1,41 +1,20 @@
 package database
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 	"os"
-	"strconv"
 
-	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-var Db *sqlx.DB
-
-func ConnectDatabase() {
-	err := godotenv.Load()
+func ConnectDatabase() *sql.DB {
+	connStr := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		log.Fatal(err)
 	}
 
-	host := os.Getenv("HOST")
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	user := os.Getenv("USER")
-	dbname := os.Getenv("DB_NAME")
-	password := os.Getenv("DB_PW")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-		host, port, user, dbname, password)
-
-	db, err := sqlx.Open("postgres", psqlInfo)
-	if err != nil {
-		fmt.Println("Error connecting to the database:", err)
-		panic(err)
-	}
-
-	Db = db
-	fmt.Println("Connected to the database successfully.")
+	return db
 }
-
-func CloseDatabase() error { return Db.Close() }
